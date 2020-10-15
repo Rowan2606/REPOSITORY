f2aecac12f4fc545e37b620b13fab1e8b7fddad8
Rowan2606 <22FrancR@mbbc.qld.edu.au>
2020-10-16 09:38:52 +1000

    Test

---
+++ HEllow World.py
@@ -0,0 +1,5 @@
+
+from sense_hat import SenseHat
+
+sense = SenseHat()
+sense.show_message("mm yes wazinga. Sean it is I al dusty i have taken physical form to speak to you")
---
+++ W2L1E3.py
@@ -0,0 +1,10 @@
+from sense_hat import SenseHat
+
+sense = SenseHat()
+
+black = (0,0,255)
+red = (255,0,0)
+
+sense.show_message("OgAoGBAGA!", text_colour=red, back_colour=black)
+
+
